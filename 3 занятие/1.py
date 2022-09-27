8aeeb6efba1e6ab916322815c2e0e427438231f6
MihailCoderIDLE <114363594+MihailCoderP@users.noreply.github.com>
2022-09-27 21:11:02 +0300

    Delete 3 занятие directory

--- 3 занятие/1.py
+++
@@ -1,7 +0,0 @@
-def firstEx():
-    a = input("Введите первое число: ")
-    b = input("Введите второе число: ")
-    c = input("Введите третье число: ")
-    sum = int(a)+int(b)+int(c)
-    print("Сумма введенных чисел:%s"%sum)
-firstEx()
