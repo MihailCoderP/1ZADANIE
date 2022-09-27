8f03e462963170fc33812adbbf28916c96b744bd
MihailCoderIDLE <114363594+MihailCoderP@users.noreply.github.com>
2022-09-27 21:10:45 +0300

    Delete 1 занятие directory

--- 1 занятие/6.py
+++
@@ -1,7 +0,0 @@
-x = float(input("введите х: "))
-y= float(input("введите y: "))
-a = x
-b = y
-y = a
-x = b
-print("x: ",x,"; y: ",y)
--- 1 занятие/7.py
+++
@@ -1,4 +0,0 @@
-number = int(input('введите число: '))
-if (number%2==0):
-    print("чётное")
-else: print(" нечётное")
